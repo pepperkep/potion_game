062879c7eb796c62073a56cb269959eec7a99afc
Austin Keppers <akeppers21@gmail.com>
2019-05-29 17:46:29 -0400

    Initial commit

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+
+    // Whether the player is dragging the object
+    private bool isDragged = false;
+    // Offset between the mouse position and the object position
+    private Vector3 offset = Vector3.zero;
+    // Camera mouse coordinates are in reference to
+    [SerializeField] private Camera dragCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set object position to mouse position when being dragged
+        if(isDragged)
+        {
+            Vector3 worldMousePosition = dragCamera.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = worldMousePosition + offset;
+        }
+    }
+
+    // Set values when player drags offset
+    void OnMouseDown()
+    {
+        isDragged = true;
+        Vector3 worldMousePosition = dragCamera.ScreenToWorldPoint(Input.mousePosition);
+        offset = transform.position - worldMousePosition;
+    }
+
+    // Release object when player lets up mouse
+    void OnMouseUp()
+    {
+        isDragged = false;
+    }
+}
