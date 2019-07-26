446d50629875991d66e5a37b6df627502473fee1
Austin Keppers <akeppers21@gmail.com>
2019-07-25 22:01:15 -0400

    Added poison bullet

---
+++ Assets/Scripts/Enemy Effects/DotEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotEffect : TempEffect
+{
+
+    [SerializeField] private float damagePerTick = 0.05f;
+    public override void FindTarget()
+    {
+        if(transform.parent != null)
+        {
+            targetComponent = transform.parent.GetComponent<Damageable>();
+        }
+        else
+        {
+            targetComponent = null;
+        }
+    }
+
+    public override void ApplyTick()
+    {
+        if(targetComponent != null)
+        {
+            ((Damageable)targetComponent).CurrentHealth -= damagePerTick;
+        }
+    }
+
+    public override void EndEffect()
+    {
+
+    }
+}
